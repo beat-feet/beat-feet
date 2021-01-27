f2904dd66181b653012bb0bd1dbf5cd6c2e33219
Peter Serwylo <peter@serwylo.com>
2021-01-28 07:43:22 +1100

    Add flourish to HUD when multiplier increases.

--- core/src/com/serwylo/beatgame/HUD.kt
+++ core/src/com/serwylo/beatgame/HUD.kt
@@ -1,15 +1,17 @@
 package com.serwylo.beatgame
 
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.*
 import com.badlogic.gdx.scenes.scene2d.ui.*
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import com.serwylo.beatgame.entities.Player
+import kotlin.math.floor
 
 class HUD(atlas: TextureAtlas) {
 
@@ -24,12 +26,14 @@ class HUD(atlas: TextureAtlas) {
     private val textureHeartEmpty: TextureRegionDrawable
     private val textureScore: TextureRegion
     private val textureDistance: TextureRegion
+    private val labelStyle: Label.LabelStyle
 
     private val heartImages: MutableList<Image> = mutableListOf()
 
     private val distanceLabel: Label
     private val scoreLabel: Label
     private val healthLabel: Label
+    private val bottomWidget: HorizontalGroup
 
     init {
 
@@ -42,7 +46,7 @@ class HUD(atlas: TextureAtlas) {
         textureScore = atlas.findRegion("score")
         textureDistance = atlas.findRegion("right_sign")
 
-        val labelStyle = Label.LabelStyle(font, Color.WHITE)
+        labelStyle = Label.LabelStyle(font, Color.WHITE)
 
         val healthWidget = HorizontalGroup()
         healthWidget.space(padding / 2)
@@ -61,7 +65,7 @@ class HUD(atlas: TextureAtlas) {
         distanceLabel = Label("", labelStyle)
         scoreLabel = Label("", labelStyle)
 
-        val bottomWidget = HorizontalGroup()
+        bottomWidget = HorizontalGroup()
         bottomWidget.space(padding / 2)
         bottomWidget.addActor(Image(textureDistance))
         bottomWidget.addActor(distanceLabel)
@@ -76,32 +80,67 @@ class HUD(atlas: TextureAtlas) {
         table.add(bottomWidget).bottom().left()
 
         stage.addActor(table)
+
     }
+
+    private var previousMultiplier = 1f
 
     fun render(distancePercent: Float, player: Player) {
         val distance = (distancePercent * 100).toInt().toString() + "%"
-        val multiplier = if (player.scoreMultiplier <= 1) "" else " x ${player.scoreMultiplier}"
-        val score = "${player.getScore()}$multiplier"
+        val multiplier = if (player.scoreMultiplier <= 1) "" else " x ${player.scoreMultiplier.toInt()}"
 
         healthLabel.setText(player.getHealth())
         distanceLabel.setText(distance)
-        scoreLabel.setText(score)
+        scoreLabel.setText("${player.getScore()}$multiplier")
 
         heartImages.forEachIndexed { i, image ->
             val fullThreshold = (i + 1) * 20
             val halfThreshold = fullThreshold - 10
 
-            image.drawable = if (player.getHealth() >= fullThreshold) {
-                textureHeartFull
-            } else if (player.getHealth() >= halfThreshold) {
-                textureHeartHalf
-            } else {
-                textureHeartEmpty
+            image.drawable = when {
+                player.getHealth() >= fullThreshold -> textureHeartFull
+                player.getHealth() >= halfThreshold -> textureHeartHalf
+                else -> textureHeartEmpty
+            }
+        }
+
+        // Bring the increasing multiplier to the players attention by showing
+        // a floating up, increasing size, reducing alpha label explaining the new multiplier.
+        if (previousMultiplier != player.scoreMultiplier) {
+            previousMultiplier = player.scoreMultiplier
+
+            // Only show feedback for whole numbers.
+            if (player.scoreMultiplier > 1f && floor(player.scoreMultiplier) == player.scoreMultiplier) {
+                stage.addActor(createIncreasedMultiplier(player.scoreMultiplier))
             }
         }
 
         stage.act()
         stage.draw()
+    }
+
+    private fun createIncreasedMultiplier(scoreMultiplier: Float): Actor {
+        val label = Container<Label>(Label("x ${scoreMultiplier.toInt()}", labelStyle))
+        label.isTransform = true
+
+        label.addAction(
+                sequence(
+                        parallel(
+                                alpha(0.3f, 1f),
+                                scaleBy(3f, 3f, 1f),
+                                moveBy(0f, scoreLabel.height * 4, 1f)
+                        ),
+                        removeActor()
+                )
+        )
+
+        // Weird using the score labels height here, but we don't have an exact place to measure this
+        // yet, so just sort of guessing. It doesn't really matter, because the animation of this
+        // actor makes it hard to see exactly where it started.
+        label.x = padding + bottomWidget.width - scoreLabel.height
+        label.y = padding
+
+        return label
     }
 
 }
